c6917b50a8c6e8b108d37765105bd9caf12baf99
Riverapjj <keko.rai.jr@gmail.com>
2022-02-12 21:29:55 -0600

    Vista CRUD

--- views/equipos_view.php
+++ views/equipos_view.php
@@ -1,48 +1,54 @@
 <!DOCTYPE html>
-<html lang="es">
-
+<html>
 <head>
-    <meta charset="UTF-8" />
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
-        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Equipos de fútbol</title>
+    <meta charset="utf-8">
+    <title>Calculadora de CUM</title>
+    <link rel="stylesheet" type="text/css" href="views/assets/css/bootstrap.css">
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/alertify.min.css"/>
+    <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/alertifyjs@1.13.1/build/css/themes/bootstrap.rtl.min.css"/>
 </head>
-
 <body>
-    <div class="container">
-        <h1 class="page-header text-center">Equipos de fútbol</h1>
-        <div class="row">
-            <div class="col-sm-8 col-sm-offset-2">
-            <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#agregarEquipo">Agregar equipo</button>
+    <?php
+       // session_start();
+    ?>
+<div class="container">
+    <h1 class="page-header text-center">Calculadora de CUM</h1>
+    <div class="row">
+        <div class="col-sm-8 col-sm-offset-2">
+            <a href="#addnew" class="btn btn-primary" data-toggle="modal" data-bs-target="#addnew"><span class="glyphicon glyphicon-plus"></span> Agregar materia</a>
+            
+            <table class="table table-bordered table-striped" style="margin-top:20px;">
+                <thead>
+                    <th>Nombe del equipo</th>
+                    <th>Liga</th>
+                    <th>Acciones</th>
+                </thead>
+                <tbody>
+                    <?php
+                    
+                        foreach ($data as $equipo) {
+                            
+                    ?>
 
-                <table class="table table-bordered table-striped" style="margin-top:20px;">
-                    <thead>
-                        <th>Equipo</th>
-                        <th>Liga</th>
-                        <th>Acciones</th>
-                    </thead>
-                    <tbody>
-                        <?php
-                            foreach ($data as $equipo) {
-                       
-                        ?>
                         <tr>
-                            <td><?=$equipo['nombreEquipo']?></td>
-                            <td><?=$equipo['nombreLiga']?></td>
+                            <td><?= $equipo['nombreEquipo'];?></td>
+                            <td><?= $equipo['nombreLiga'];?></td>
                             <td>
                                 <a href="#update_<?php echo $equipo['idEquipo']; ?>" data-toggle="modal" class="btn btn-success">Editar</a>
                                 <a href="#delete_<?php echo $equipo['idEquipo']; ?>" data-toggle="modal" class="btn btn-danger">Borrar</a>
                             </td>
                         </tr>
 
-                        <?php
-                            }
-                        ?>
-                    </tbody>
-                </table>
-            </div>
+                    <?php
+                        }
+                    ?>
+                </tbody>
+            </table>
         </div>
     </div>
-    <?php include('nuevo_equipo.php')?>
+</div>
+<?php include('views/nuevo_equipo.php'); ?>
+<script src="views/assets/js/jquery.min.js"></script>
+<script src="views/assets/js/bootstrap.min.js"></script>
 </body>
 </html>
