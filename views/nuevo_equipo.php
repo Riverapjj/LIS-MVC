cd564bf4a3e5ed8486a0152364a90eab0de5ac2d
Riverapjj <keko.rai.jr@gmail.com>
2022-02-12 21:31:44 -0600

    Revert "Vista CRUD"
    This reverts commit c6917b50a8c6e8b108d37765105bd9caf12baf99.

--- views/nuevo_equipo.php
+++ views/nuevo_equipo.php
@@ -1,15 +1,13 @@
-<!-- Nueva materia -->
-<div class="modal fade" id="addnew" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
+<!-- Modal -->
+<div class="modal fade" id="agregarEquipo" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
     <div class="modal-dialog">
         <div class="modal-content">
             <div class="modal-header">
-                <button type="button" class="close" data-dismiss="modal" aria-hidden="true">&times;</button>
-                <center><h4 class="modal-title" id="myModalLabel">Nueva materia</h4></center>
+                <h5 class="modal-title" id="staticBackdropLabel">Modal title</h5>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
             <div class="modal-body">
-			<div class="container-fluid">
-			<form method="POST" action="agregar.php">
-				<div class="row form-group">
+            <div class="row form-group">
 					<div class="col-sm-2">
 						<label class="control-label" for="codigo">Codigo:</label>
 					</div>
@@ -25,30 +23,16 @@
 						<input type="text" class="form-control" name="nombre" id="nombre">
 					</div>
 				</div>
-				<div class="row form-group">
-					<div class="col-sm-2">
-						<label class="control-label" for="uvs">UVS:</label>
-					</div>
-					<div class="col-sm-10">
-						<input type="number" min="2" max="5" class="form-control" name="uvs" id="uvs">
-					</div>
-				</div>
-				<div class="row form-group">
-					<div class="col-sm-2">
-						<label class="control-label" for="nota" >Nota:</label>
-					</div>
-					<div class="col-sm-10">
-						<input type="number" min="0" max="10" step="0.1" class="form-control" name="nota" id="nota">
-					</div>
-				</div>
             </div> 
 			</div>
             <div class="modal-footer">
-                <button type="button" class="btn btn-default" data-dismiss="modal"><span class="glyphicon glyphicon-remove"></span> Cancelar</button>
-                <button type="submit" name="add" class="btn btn-primary"><span class="glyphicon glyphicon-floppy-disk"></span> Agregar</a>
-			</form>
+                <button type="button" class="btn btn-default" data-dismiss="modal"> Cancelar</button>
+                <button type="submit" name="add" class="btn btn-primary">Agregar</a>
             </div>
- 
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                <button type="button" class="btn btn-primary">Understood</button>
+            </div>
         </div>
     </div>
 </div>
